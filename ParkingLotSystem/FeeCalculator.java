dee235c25320a325127eb93e592ddd64b6d25760
Manoj-Kiyan <manojkiyan2k4@gmail.com>
2025-09-04 09:21:00 +0530

    Delete ParkingLotSystem directory

--- ParkingLotSystem/FeeCalculator.java
+++
@@ -1,9 +0,0 @@
-
-import java.time.LocalDateTime;
-
-public interface FeeCalculator{
-
-        double calculate(LocalDateTime inTime,LocalDateTime outTime);
-
-    }
-
--- ParkingLotSystem/VehicleType.java
+++
@@ -1,8 +0,0 @@
-public enum VehicleType {
-
-    BIKE, CAR, TRUCK, BICYCLE;
-    
-}
-
-    
-
